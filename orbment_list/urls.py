6da888fe24d299764d562151270df652562ef950
Joe Langford <joseph.langford@gmail.com>
2023-08-01 00:19:32 -0400

    basic site, time for bed

--- orbment_list/urls.py
+++ orbment_list/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('orbments/', views.OrbmentListView.as_view(), name='orbments'),
+    path('orbment/<int:pk>', views.OrbmentDetailView.as_view(), name='orbment-detail'),
 ]
+
--- orbment_list/views.py
+++ orbment_list/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Element, AttributeType, Attribute, Orbment, Power, Range
 # Create your views here.
+
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_orbments = Orbment.objects.all().count()
+
+    context = {
+        'num_orbments': num_orbments,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+
+class OrbmentListView(generic.ListView):
+    model = Orbment
+    paginate_by = 10
+
+class OrbmentDetailView(generic.DetailView):
+    model = Orbment
